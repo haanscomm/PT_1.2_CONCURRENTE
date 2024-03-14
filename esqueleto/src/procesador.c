438f420fee5c4ff6702c417fb481590a8b4ffd37
angelaajiao <angelajiao04@gmail.com>
2024-03-14 16:51:46 +0100

    procesador prueba

--- esqueleto/src/procesador.c
+++ esqueleto/src/procesador.c
@@ -11,7 +11,6 @@ int main(int argc, char *argv[])
     exit(EXIT_FAILURE);
   }
   
-  return EXIT_SUCCESS;
 
   // Llamar a una función que me diga en que líneas existe el patron
   procesar_linea(argv[1], argv[2]);
@@ -21,16 +20,32 @@ int main(int argc, char *argv[])
 
 void procesar_linea(char *fichero, char *patron)
 {
-
+    char linea[256];
+    int ch;
     // Declaramos numlinea
     int numLinea = 1;
 
     // Abrir el archivo
+    FILE *archivo = fopen(*fichero, "r");
 
+    if (archivo == NULL)
+    {
+      fprintf(stderr, "Error al abrir el fichero. \n", *fichero);
+      exit(EXIT_FAILURE);
+    }
+    
 
     // Hacer un bucle que vaya leyendo linea a linea 
-
-
+    while (fgets(linea, sizeof(linea), archivo) != NULL)
+    {
+      //Busca la palabra en la linea
+      if (strstr(linea, ))
+      {
+        /* code */
+      }
+      
+    }
+    
     // Dentro del bucle, con strstr buscar el patron dentro de la linea
     // Si existe, tenemos que imprimir esto:
     printf("[PROCESADOR %d] Patron '%s' encontrado en linea %d\n",getpid(),patron,numLinea);
