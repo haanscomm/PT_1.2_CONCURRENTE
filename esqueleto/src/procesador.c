c97e42b9645cb2292bea3e9b729b42d232d180c8
angelaajiao <angelajiao04@gmail.com>
2024-03-15 20:39:52 +0100

    procesador

--- esqueleto/src/procesador.c
+++ esqueleto/src/procesador.c
@@ -1,6 +1,12 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
+
+#define PATH_MAX 4096
+
+// Declaración de la función procesar_linea
+void procesar_linea(char *fichero, char *patron);
 
 int main(int argc, char *argv[])
 {
@@ -11,7 +17,6 @@ int main(int argc, char *argv[])
     exit(EXIT_FAILURE);
   }
   
-
   // Llamar a una función que me diga en que líneas existe el patron
   procesar_linea(argv[1], argv[2]);
 
@@ -20,37 +25,37 @@ int main(int argc, char *argv[])
 
 void procesar_linea(char *fichero, char *patron)
 {
-    char linea[256];
-    int ch;
     // Declaramos numlinea
     int numLinea = 1;
 
     // Abrir el archivo
-    FILE *archivo = fopen(*fichero, "r");
+    FILE *fp;
+    char linea[PATH_MAX];
 
-    if (archivo == NULL)
-    {
-      fprintf(stderr, "Error al abrir el fichero. \n", *fichero);
-      exit(EXIT_FAILURE);
-    }
-    
+    if ((fp = fopen(fichero, "r")) == NULL)
+  {
+    fprintf(stderr, "Error al abrir el fichero %s\n", fichero);
+    exit(EXIT_FAILURE);
+  }
 
     // Hacer un bucle que vaya leyendo linea a linea 
-    while (fgets(linea, sizeof(linea), archivo) != NULL)
-    {
-      //Busca la palabra en la linea
-      if (strstr(linea, ))
-      {
-        /* code */
-      }
-      
-    }
-    
+  while (fgets(linea, sizeof(linea), fp) != NULL)
+  {
+    //Busca la palabra en la linea
     // Dentro del bucle, con strstr buscar el patron dentro de la linea
+    if (strstr(linea, patron) != NULL)
+    {
     // Si existe, tenemos que imprimir esto:
     printf("[PROCESADOR %d] Patron '%s' encontrado en linea %d\n",getpid(),patron,numLinea);
+      
+    }
+    numLinea++;
+     
+  }
+    
 
     // Cerrar el fichero
+    fclose(fp);
 
 }
 
